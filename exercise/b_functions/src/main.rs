44e8a43751da8cf329cce046e9b551243d1ba8bd
utsavgoswami <utsavg19@googlemail.com>
2023-10-07 13:25:16 -0400

    Fix area fn

--- exercise/b_functions/src/main.rs
+++ exercise/b_functions/src/main.rs
@@ -30,7 +30,7 @@ fn area_of(x: i32, y: i32) -> i32 {
     // 2a. Fix this function to correctly compute the area of a rectangle given
     // dimensions x and y by multiplying x and y and returning the result.
     //
-    return 0;
+    x * y
     // Challenge: It isn't idiomatic (the normal way a Rust programmer would do things) to use
     //            `return` on the last line of a function. Change the last line to be a
     //            "tail expression" that returns a value without using `return`.
