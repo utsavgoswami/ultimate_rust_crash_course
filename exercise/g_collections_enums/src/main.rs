ea1eed8c953ac6a47738cd5a18c48cbcf3c6afaa
utsavgoswami <utsavg19@googlemail.com>
2023-10-08 00:52:41 -0400

    Impl points

--- exercise/g_collections_enums/src/main.rs
+++ exercise/g_collections_enums/src/main.rs
@@ -7,6 +7,12 @@
 // - `Bullseye`
 // - `Hit`, containing the distance from the center (an f64)
 // - `Miss`
+enum Shot {
+    Bullseye,
+    Hit(f64),
+    Miss
+}
+
 //
 // You will need to complete 1b as well before you will be able to run this program successfully.
 
@@ -14,10 +20,23 @@ impl Shot {
     // Here is a method for the `Shot` enum you just defined.
     fn points(self) -> i32 {
         // 1b. Implement this method to convert a Shot into points
-        // - return 5 points if `self` is a `Shot::Bullseye`
-        // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
-        // - return 1 point if `self` is a `Shot::Hit(x)` where x >= 3.0
-        // - return 0 points if `self` is a Miss
+        let score = match self {
+            // - return 5 points if `self` is a `Shot::Bullseye`
+            Shot::Bullseye => {
+                5
+            },
+            Shot::Hit(x) => {
+                // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
+                // - return 1 point if `self` is a `Shot::Hit(x)` where x >= 3.0
+                let res = if x >= 3.0 { 1 } else { 2 };
+                res
+            },
+            Shot::Miss => {
+                // - return 0 points if `self` is a Miss
+                0
+            }
+        };
+        score
     }
 }
 
